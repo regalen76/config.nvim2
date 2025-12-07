b2b994df2e51058b9060c910019ac2e4eca2c470
Reonaldo Galen Maliode <reonify@Reonaldos-MacBook-Air.local>
2025-12-07 12:41:48 +0700

    lazy event

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,5 +1,6 @@
 return {
   "nvim-lualine/lualine.nvim",
+  event = "VeryLazy",
   config = function()
     local colors = {
       blue = "#80a0ff",
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,7 +1,7 @@
 return {
 	"nvim-treesitter/nvim-treesitter",
 	branch = "master",
-	lazy = false,
+	event = { "BufReadPost", "BufWritePost", "BufNewFile", "VeryLazy" },
 	opts = {
 		ensure_installed = {
 			"lua",
