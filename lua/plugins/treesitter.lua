9716227e3a857d75665e781344234010797686d0
Reonaldo Galen Maliode <reonify@Reonaldos-MacBook-Air.local>
2025-12-08 19:09:21 +0700

    cleaner treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -2,7 +2,14 @@ return {
 	"nvim-treesitter/nvim-treesitter",
 	branch = "master",
 	event = { "BufReadPost", "BufWritePost", "BufNewFile", "VeryLazy" },
+
+	-- make sure textobjects is installed
+	dependencies = {
+		"nvim-treesitter/nvim-treesitter-textobjects",
+	},
+
 	opts = {
+		-- these are your existing settings
 		ensure_installed = {
 			"lua",
 			"vue",
@@ -22,8 +29,39 @@ return {
 		indent = {
 			enable = true,
 		},
+
+		-- add textobjects here
+		textobjects = {
+			move = {
+				enable = true,
+				set_jumps = true, -- whether to set jumps in the jumplist
+
+				goto_next_start = {
+					["]f"] = "@function.outer",
+					["]c"] = "@class.outer",
+					["]a"] = "@parameter.inner",
+				},
+				goto_next_end = {
+					["]F"] = "@function.outer",
+					["]C"] = "@class.outer",
+					["]A"] = "@parameter.inner",
+				},
+				goto_previous_start = {
+					["[f"] = "@function.outer",
+					["[c"] = "@class.outer",
+					["[a"] = "@parameter.inner",
+				},
+				goto_previous_end = {
+					["[F"] = "@function.outer",
+					["[C"] = "@class.outer",
+					["[A"] = "@parameter.inner",
+				},
+			},
+		},
 	},
+
 	build = ":TSUpdate",
+
 	config = function(_, opts)
 		require("nvim-treesitter.configs").setup(opts)
 	end,
